801c1bae679114651bddc90b35f87b360a225ab7
Mauricio Quezada <waxkun@gmail.com>
2020-01-08 19:13:04 -0300

    create tables

--- create_tables.py
+++ create_tables.py
@@ -1,8 +1,8 @@
 from config.settings import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 from db.models import Base
 from sqlalchemy import create_engine
-
+from db.db_manager import engine
 
 if __name__ == "__main__":
-    engine = create_engine(f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4")
+    #engine = create_engine(f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4")
     Base.metadata.create_all(engine)
